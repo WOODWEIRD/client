9fe7311e3ef6bc504a348c90cfe43ebfab5ef3d5
nassar <nassarr812@gmail.com>
2024-02-12 16:13:44 +0300

    end of 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { error } from 'console';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'dotnet app';
+  users: any;
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => { this.users = response },
+      error: error => console.log(error),
+      complete: () => console.log("req completed")
+
+
+    })
+  }
+
+
 }
